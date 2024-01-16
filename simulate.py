fb855c2bacf8d10a0d69261289ec89724f86b314
rohbot25 <ewrohan@gmail.com>
2024-01-16 18:09:30 -0500

    Starting

--- simulate.py
+++ simulate.py
@@ -1,5 +1,10 @@
 import pybullet as p
+import time
 
 physicsClient = p.connect(p.GUI)
-p.stepSimulation()
+for i in range(1000):
+    time.sleep(1/60)
+    print(i)
+    p.stepSimulation()
+
 p.disconnect()
