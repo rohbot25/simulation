4d28ec2c9bf34f4a1cd157624b4cd5dabaa16f64
rohbot25 <ewrohan@gmail.com>
2024-01-18 23:46:52 -0500

    assignment 3

--- generate.py
+++ generate.py
@@ -8,13 +8,22 @@ x = 0
 y = 0.5
 z = 0
 pyrosim.Start_SDF("boxes.sdf")
-for i in range(10):
-    pyrosim.Send_Cube(name="Box", pos=[x,z,y] , size=[length,width,height])
-    length*=.9
-    width*=.9
-    previousHeight = .5 * height
-    height*=.9
-    y = y + previousHeight + .5 * height
+for i in range(5):
+    x = 0
+    for j in range(5):
+        length = 1
+        width = 1
+        height = 1
+        y = 0.5
+        x += 1
+        for k in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[x,z,y] , size=[length,width,height])
+            length*=.9
+            width*=.9
+            previousHeight = .5 * height
+            height*=.9
+            y = y + previousHeight + .5 * height
+    z += 1
 
 
 pyrosim.End()
