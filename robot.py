b9aefdca79102d2a14d0eddf32148a60bc2cd517
rohbot25 <ewrohan@gmail.com>
2024-02-12 15:49:37 -0500

    joint angle

--- robot.py
+++ robot.py
@@ -32,7 +32,7 @@ class ROBOT:
             if(self.nn.Is_Motor_Neuron(neuronName)):
                 jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
                 desiredAngle = self.nn.Get_Value_Of(neuronName)
-                self.motors[jointName].Set_Value(desiredAngle,self.robotId)
+                self.motors[jointName].Set_Value(desiredAngle*c.motorJointRange,self.robotId)
     def Think(self,t):
         self.nn.Update()
         #self.nn.Print()
